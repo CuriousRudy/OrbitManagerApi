07dc9df20166bb4d33d32766550a8a70af84e796
Abe Rudy <aberudy@gmail.com>
2018-01-09 02:17:24 -0500

    removed duplicate controllers, comment out Auth controller for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :forums
-  resources :messages
-  resources :memberships
-  resources :clans
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :forums
+      resources :messages
+      resources :memberships
+      resources :clans
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
